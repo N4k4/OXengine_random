fe4e6aedf2f711ab2ccd34b8333e3e00041e6475
N4k4 <61612408+N4k4@users.noreply.github.com>
2022-06-28 21:29:14 +0900

    初期設定

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Programs
+{
+    
+    class Program
+    {
+        const string version = "0.0.0";
+        static void Main(string[] args)
+        {
+
+            setup();
+
+            Console.WriteLine("Hello World!");
+        }
+
+        
+        //起動時処理
+        static void setup(){
+            Console.WriteLine("OXengine_random version:{0}",version);
+            //Environment.Exit(1);
+            return;
+        }
+    }
+}
+
